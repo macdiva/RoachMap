d824a9a662421bb0ae38e32dbf7a9da79171366d
John Myles White <jmw@johnmyleswhite.com>
2010-11-06 20:25:55 -0400

    Remove old data files during update

--- get_and_parse_data.sh
+++ get_and_parse_data.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
+cd /home/roachmap/backend
+rm data/*
 curl http://www.nyc.gov/html/doh/downloads/zip/bigapps/dohmh_restaurant-inspections_002.zip \
   > data/dohmh_restaurant-inspections_002.zip
 cd data/
 unzip dohmh_restaurant-inspections_002.zip
 cd ..
-cat data/WebExtract.txt | python2.6 roach_parser.py 30 0 > data/binomial_roaches.tsv
+cat data/WebExtract.txt | python2.6 roach_parser.py 90 0 > data/binomial_roaches.tsv
