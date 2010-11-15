6c526e0d9f43ac9689c6b999f8771648fb4da215
Max Shron <max.shron@gmail.com>
2010-11-14 19:13:02 -0500

    Rewrote roach_parser.py to correctly count restaurants.

--- roach_parser.py
+++ roach_parser.py
@@ -1,56 +1,44 @@
-#!/usr/bin/python2.6
+import sys
+import csv
+import datetime
 
-# RoachMap backend parser
-# John Myles White
-# 11/6/2010
+# Rewritten by Max Shron from code by John Myles White
 
 # ARGV
 # ARGV[1] == Window Size
 # ARGV[2] == Window End Offset from Today
 
-# Pass in an integer, which is the number of days before now during
-# which incidents are counted.
+roachcode = "04M"
 
-# Loops over lines, only considers lines with VIOLCODE == '04M'
-# Outputs a tab-separated file with zipcodes and DBA.
+window = int(sys.argv[1])
+back = int(sys.argv[2])
 
-import sys
-import csv
-import re
-from datetime import *
+datestring = "%Y-%m-%d %H:%M:%S"
+now = datetime.datetime.now()
 
-window_size = int(sys.argv[1])
-window_end = int(sys.argv[2])
+#key: zip, value: {restaurant,code}
+zipcodes = {}
 
-input = csv.reader(sys.stdin)
+for data in csv.reader(sys.stdin):
+    if data[0] == 'CAMIS': continue
+    d = now - datetime.datetime.strptime(data[8],datestring)
+    d = d.days
+    if window+back > d > back:
+       camis = data[0]
+       zipcode = data[5]
+       code = data[10] 
+       zipcodes.setdefault(zipcode,{}).setdefault(camis,[]).append(code)
 
-total_inspections = {}
-roach_findings = {}
+zipcodes_out = {}
 
-for row in input:
-    try:
-        incident_dba = row[1]
-        incident_zipcode = row[5]
-        incident_timestamp = row[8]
-        incident_violation_code = row[10]
-        incident_datetime = datetime.strptime(incident_timestamp, '%Y-%m-%d %H:%M:%S')
-        temporal_distance = date.today()- incident_datetime.date()
-        if temporal_distance <= timedelta(window_size + window_end) and temporal_distance >= timedelta(window_end):
-            if total_inspections.has_key(incident_zipcode):
-                total_inspections[incident_zipcode] = total_inspections[incident_zipcode] + 1
-            else:
-                total_inspections[incident_zipcode] = 1
-            if incident_violation_code == '04M':
-                if roach_findings.has_key(incident_zipcode):
-                    roach_findings[incident_zipcode] = roach_findings[incident_zipcode] + 1
-                else:
-                    roach_findings[incident_zipcode] = 1
-    except:
-        1 # We're just ignoring errors for time being.
+for z in zipcodes:
+    total = 0
+    roach = 0
+    for r in zipcodes[z]:
+       total += 1
+       if roachcode in zipcodes[z][r]:
+          roach += 1
+    zipcodes_out[z] = [str(total),str(roach)]
 
-for zipcode in total_inspections.keys():
-    if not re.search(r'\d{5}', zipcode):
-        continue
-    if not roach_findings.has_key(zipcode):
-        roach_findings[zipcode] = 0
-    print "\t".join([zipcode, str(total_inspections[zipcode]), str(roach_findings[zipcode])])
+for z,v in zipcodes_out.iteritems():
+    print ','.join([z] + v)
